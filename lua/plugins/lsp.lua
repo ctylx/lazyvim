fd30b0a4aa84ceaa0626270d1456a49352f2cd47
ctylx <chenty0514@gmail.com>
2024-04-12 22:24:09 +0800

    add LazyVim scripts

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,13 @@
+return {
+  -- nvim-lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      setup = {
+        clangd = function(_, opts)
+          opts.capabilities.offsetEncoding = { "utf-16" }
+        end,
+      },
+    },
+  },
+}
