980b1b0c5dac2a8d0bbe276187dad323095b79a5
Tengyu Chen <chenty0514@gmail.com>
2024-10-14 21:19:11 +0800

    新增python lsp配置

--- init.lua
+++ init.lua
@@ -1,2 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- 设置最大字符数
+vim.opt_local.textwidth = 120
+-- 启用自动换行
+vim.opt_local.formatoptions:append("t")
+
+-- 禁用当前缓冲区的自动格式化
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = {"python"},
+    callback = function()
+        vim.b.autoformat = false
+    end,
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+vim.opt.guifont = "FiraCode Nerd Font:h16"
 
 -- provider
 vim.g.python3_host_prog = "/Users/ctylx/miniconda3/bin/python"
@@ -14,3 +15,9 @@ vim.g.neovide_refresh_rate_idle = 5
 vim.g.neovide_fullscreen = true
 vim.g.neovide_remember_window_size = true
 vim.g.neovide_cursor_vfx_mode = "pixiedust"
+
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "pyright"
+-- Set to "ruff_lsp" to use the old LSP implementation version.
+vim.g.lazyvim_python_ruff = "ruff"
