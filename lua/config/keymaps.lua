669e486a11346b7966c5e82ebf22da812b74d861
Tengyu Chen <chenty0514@gmail.com>
2025-01-21 17:14:28 +0800

    Remove unnecessary lua scripts

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- cmd + s to save
+vim.keymap.set("i", "jk", "<ESC>", { silent = true })
+vim.keymap.set({ "i", "x", "n", "s" }, "<D-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,33 +1,4 @@
 return {
-  -- flash.nvim
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    vscode = true,
-    ---@type Flash.Config
-    opts = {},
-    -- stylua: ignore
-    keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-    },
-  },
-
-  -- indent-blankline
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    commit = "29be0919b91fb59eca9e90690d76014233392bef",
-  },
-
-  -- vim-visual-multi
-  -- {
-  --   "mg979/vim-visual-multi",
-  -- },
-
   -- vim-multiple-cursors
   {
     "terryma/vim-multiple-cursors",
