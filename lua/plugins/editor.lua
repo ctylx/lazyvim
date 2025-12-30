c69aa8e77b92457f981b1130de6225a220fe18b9
Tengyu Chen <chenty0514@gmail.com>
2025-12-30 21:38:13 +0800

    enable ai.avante

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -3,10 +3,6 @@ return {
   {
     "terryma/vim-multiple-cursors",
   },
-  -- {
-  --   "echasnovski/mini.nvim",
-  --   version = false,
-  -- },
   {
     "nvim-tree/nvim-web-devicons",
     opts = {},
